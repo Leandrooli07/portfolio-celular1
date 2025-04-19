017d14e2187b8e9e1d84c32594ef102926c2b63a
Leandrooli07 <leandrooli.sousa07@gmail.com>
2025-04-19 13:41:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara")
+
+
+function mostrarModal(){
+	modal.style.left = '40%'
+	mascara.style.visibility = 'visible'
+}
+
+function esconderModal(){
+	modal.style.left = '-40%'
+	mascara.style.visibility = 'hidden'
+}
+
+
+document.getElementById("toggleButton").addEventListener("click", 
+function() {
+	
+var info = document.getElementById("info");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+        this.textContent = "SAIBA MENOS";
+    } else {
+        info.style.display = "none";
+        this.textContent = "SAIBA MAIS";
+    }
+});
+
+document.getElementById("toggleButton").addEventListener("click", 
+function() {
+	
+var info1 = document.getElementById("info1");
+    if (info1.style.display === "none") {
+        info1.style.display = "block";
+        this.textContent = "SAIBA MENOS";
+    } else {
+        info1.style.display = "none";
+        this.textContent = "SAIBA MAIS";
+    }
+});
+
